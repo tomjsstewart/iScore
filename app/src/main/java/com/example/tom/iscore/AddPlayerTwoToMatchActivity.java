a1113aaebb21481dca1ef6c97c1ef599ef9051d0
Tom Stewart <tomjsstewart@gmail.com>
2016-12-09 21:45:45 +0000

    Add files for AddPlayerTwoToMatch

---
+++ app/src/main/java/com/example/tom/iscore/AddPlayerTwoToMatchActivity.java
@@ -0,0 +1,13 @@
+package com.example.tom.iscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPlayerTwoToMatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_player_two_to_match);
+    }
+}
