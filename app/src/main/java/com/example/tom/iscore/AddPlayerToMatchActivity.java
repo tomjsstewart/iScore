dd7620800d8d4ec21d115bbc13a1f90cf3f382fa
Tom Stewart <tomjsstewart@gmail.com>
2016-12-09 21:23:26 +0000

    Create AddPlayerToMatchActivity

---
+++ app/src/main/java/com/example/tom/iscore/AddPlayerToMatchActivity.java
@@ -0,0 +1,13 @@
+package com.example.tom.iscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPlayerToMatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_player_to_match);
+    }
+}
