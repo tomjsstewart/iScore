fffc921523d76cba01b90d86abbb0ab97ed30fae
Tom Stewart <tomjsstewart@gmail.com>
2016-12-24 18:01:32 +0000

    Force ViewPlayerProfileActivity to open Portrait mode

--- app/src/main/java/com/example/tom/iscore/ViewPlayerProfileActivity.java
+++ app/src/main/java/com/example/tom/iscore/ViewPlayerProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.tom.iscore;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,6 +24,8 @@ public class ViewPlayerProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_player_profile);
+        //Force Screen to open Portrait mode
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         DBHandler db = new DBHandler(this);
 
