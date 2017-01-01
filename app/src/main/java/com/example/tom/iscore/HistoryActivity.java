b2f33fb27755db9f8dd5fc03f657b248d5410765
Tom Stewart <tomjsstewart@gmail.com>
2017-01-01 20:23:29 +0000

    Set up HistoryActivity to dispay a ListView

--- app/src/main/java/com/example/tom/iscore/HistoryActivity.java
+++ app/src/main/java/com/example/tom/iscore/HistoryActivity.java
@@ -1,5 +1,8 @@
 package com.example.tom.iscore;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,24 +10,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class HistoryActivity extends AppCompatActivity {
+public class HistoryActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Force Screen to open Portrait mode
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        displayHistoryList();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        displayHistoryList();
+    }
+
+    private void displayHistoryList()
+    {
+
     }
 
 }
